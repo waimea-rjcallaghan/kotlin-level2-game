d938e3e4ad40146d0ba3ff7a033f87c9aba24619
waimea-rjcallaghan <rjcallaghan@waimea.school.nz>
2026-03-19 12:22:10 +1300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -2,8 +2,8 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:   PROJECT NAME HERE
- * Project Author: PROJECT AUTHOR HERE
+ * Project Name:    PROJECT NAME HERE
+ * Project Author: Ryder Callaghan PROJECT AUTHOR HERE
  * GitHub Repo:    GITHUB REPO URL HERE
  * ---------------------------------------------------------------------
  * Notes:
@@ -12,8 +12,35 @@
  */
 
 
-
+val cells = mutableListOf<String?>()
 fun main() {
+    createCells()
+    showCells()
     println("Hello World!")
+}
+
+fun createCells() {
+    for (i in 1..16) {
+        cells.add("EMPTY")
+    }
+}
+
+fun showCells() {
+    for (i in 1..cells.size) {
+        print("   CELL $i  ")
+    }
+    println()
+    print("┌──────────")
+    print("┬──────────".repeat(cells.size - 1))
+    println("┐")
+
+    for (cell in cells) {
+        print("│ ${cell?.padEnd(8)} ")
+    }
+    print("│")
+    println()
+    print("└──────────")
+    print("┴──────────".repeat(cells.size - 1))
+    println("┘")
 }
 
