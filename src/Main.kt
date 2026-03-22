4393c6bf3295dea9a14af4f5c58dfc373774e477
waimea-rjcallaghan <rjcallaghan@waimea.school.nz>
2026-03-23 10:48:13 +1300

    Update Main.kt

--- src/Main.kt
+++ src/Main.kt
@@ -16,7 +16,8 @@ val cells = mutableListOf<String?>()
 fun main() {
     createCells()
     showCells()
-    
+    addCoin()
+
 }
 
 fun createCells() {
@@ -43,6 +44,50 @@ fun showCells() {
     print("└────────────")
     print("┴────────────".repeat(cells.size - 1))
     println("┘")
+}
+
+
+fun addCoin() {
+    val whiteCoin = "white"
+    val blackCoin = "black"
+
+    for (i in 1..4) {
+
+        print("Enter white coin positions: ")
+        val position = readlnOrNull()?.trim()?.uppercase()
+
+        if (position.isNullOrEmpty()) {
+            println("No position entered.")
+            return
+        }
+
+        if (position.length > 8) {
+            println("Input too long.")
+            return
+        }
+
+        println()
+
+        print("Enter Cell Number 1 to 16: ")
+        val cell = readlnOrNull()?.toIntOrNull()
+
+        if (cell == null) {
+            println("Invalid number.")
+            return
+        }
+
+        val index = cell - 1
+
+        if (index in 0..15) {
+            if (cells[index] == "EMPTY") {
+                cells[index] = position
+            } else {
+                println("Cell already occupied.")
+            }
+        } else {
+            println("Invalid Cell Number.")
+        }
+    }
 }
 
 
