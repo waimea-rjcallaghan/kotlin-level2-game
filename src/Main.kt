9180482fbf6edc6855dcb64df05bda0601347f87
waimea-rjcallaghan <rjcallaghan@waimea.school.nz>
2026-04-02 12:24:51 +1300

    added rules text
    added rules text

--- src/Main.kt
+++ src/Main.kt
@@ -2,8 +2,8 @@
  * =====================================================================
  * Programming Project for NCEA Level 2, Standard 91896
  * ---------------------------------------------------------------------
- * Project Name:    PROJECT NAME HERE
- * Project Author: Ryder Callaghan PROJECT AUTHOR HERE
+ * Project Name: Pinned DTD project
+ * Project Author: Ryder Callaghan
  * GitHub Repo:    GITHUB REPO URL HERE
  * ---------------------------------------------------------------------
  * Notes:
@@ -15,8 +15,53 @@
 val cells = mutableListOf<String?>()
 var blackCell = 0
 fun main() {
+
+    var ruleChoice = null
+
+    while (ruleChoice != "y, n") {
+        println("[Y]es")
+        println("[N]o")
+        println("Would you like to see the game rules? ")
+        var ruleChoice = readln()
+        if (ruleChoice == "y") {
+            println("Pinned \uD83D\uDCCC \n" +
+                    "\n" +
+                    "Game Setup \n" +
+                    "\n" +
+                    "A row of 16 squares, numbered 1 to 16 from left to right \n" +
+                    "\n" +
+                    "5 counters (total) are placed randomly on the board - 4 white and 1 black  \n" +
+                    "\n" +
+                    "Decide who goes first \n" +
+                    "\n" +
+                    "Gameplay \n" +
+                    "\n" +
+                    "Players take turns - You may not skip your turn \n" +
+                    "\n" +
+                    "On your turn you must do exactly one of the following: \n" +
+                    "\n" +
+                    "Slide any counter (black or white) any number of squares to the left, as long as no other counter is in the way and the destination square is empty, or… \n" +
+                    "\n" +
+                    "Remove the counter on square 1 (only if a counter is there) \n" +
+                    "\n" +
+                    "Win Condition \n" +
+                    "\n" +
+                    "The player who removes the black counter from square 1 wins \n" +
+                    "\n" +
+                    "Variant \n" +
+                    "\n" +
+                    "Counters can slide either left or right (but still can’t jump other counters) ")
+            break
+        }
+        if (ruleChoice == "n") {
+            break
+        }
+
+    }
+
     print("Enter your name player one: ")
     val player1 = readln()
+
     print("Enter your name player two: ")
     val player2 = readln()
     createCells()
@@ -61,11 +106,11 @@ fun main() {
             "r" -> removeCoin()
         }
         if (blackCell == 1){
-            println("|----------------------------------------------------------|")
-            println("|                                                          |")
+            println("|--------------------------------------------------|")
+            println("|                                                  |")
             println("|${"you win ($player2) playertwo".padEnd(length = 50)}|")
-            println("|                                                          |")
-            println("|----------------------------------------------------------|")
+            println("|                                                  |")
+            println("|--------------------------------------------------|")
             break
 
 
